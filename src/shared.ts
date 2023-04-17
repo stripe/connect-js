6c6b74a3453001362bc3997c24cff3529cf1b0ba
jojo-stripe <124314569+jojo-stripe@users.noreply.github.com>
2023-04-17 12:29:26 -0700

    Add type declaration files (#6)
    * add type declarations
    
    * type as wrapper
    
    * remove empty export

--- src/shared.ts
+++ src/shared.ts
@@ -1,30 +1,8 @@
-export type LoadConnect = (...args: Parameters<any>) => Promise<any | null>;
+import { StripeConnectWrapper, IStripeConnectInitParams } from "../types";
 
-type OverlayOption = "dialog" | "drawer";
-
-type UIConfigOptions = {
-  overlay?: OverlayOption;
-  overlayZIndex?: number;
-};
-
-type AppearanceOptions = {
-  colors?: {
-    primary?: string;
-  };
-  fontFamily?: string;
-};
-
-interface IStripeConnectInitParams {
-  publishableKey: string;
-  clientSecret: string;
-  appearance?: AppearanceOptions;
-  uiConfig?: UIConfigOptions;
-  refreshClientSecret?: () => Promise<string>;
-}
-
-interface StripeConnectWrapper {
-  initialize: (params: IStripeConnectInitParams) => void;
-}
+export type LoadConnect = (
+  ...args: Parameters<any>
+) => Promise<StripeConnectWrapper | null>;
 
 const EXISTING_SCRIPT_MESSAGE =
   "loadConnect was called but an existing Connect.js script already exists in the document; existing script parameters will be used";
@@ -53,9 +31,9 @@ const injectScript = (): HTMLScriptElement => {
   return script;
 };
 
-let stripePromise: Promise<any | null> | null = null;
+let stripePromise: Promise<StripeConnectWrapper | null> | null = null;
 
-export const loadScript = (): Promise<any | null> => {
+export const loadScript = (): Promise<StripeConnectWrapper | null> => {
   // Ensure that we only attempt to load Connect.js at most once
   if (stripePromise !== null) {
     return stripePromise;
---
+++ types/shared.d.ts
@@ -0,0 +1,37 @@
+export declare type LoadConnect = (
+  ...args: Parameters<any>
+) => Promise<any | null>;
+
+export declare type OverlayOption = "dialog" | "drawer";
+
+export declare type UIConfigOptions = {
+  overlay?: OverlayOption;
+  overlayZIndex?: number;
+};
+
+export declare type AppearanceOptions = {
+  colors?: {
+    primary?: string;
+  };
+  fontFamily?: string;
+};
+
+export interface IStripeConnectInitParams {
+  publishableKey: string;
+  clientSecret: string;
+  appearance?: AppearanceOptions;
+  uiConfig?: UIConfigOptions;
+  refreshClientSecret?: () => Promise<string>;
+}
+
+export interface StripeConnectWrapper {
+  initialize: (params: IStripeConnectInitParams) => void;
+}
+
+export declare const findScript: () => HTMLScriptElement | null;
+
+export declare const loadScript: () => Promise<any | null>;
+
+export declare const initStripeConnect: (
+  stripeConnectPromise: StripeConnectWrapper | null
+) => any | null;
