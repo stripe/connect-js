8168abdc1c1b705c6c8eb50e43061c8ad2034903
jojo-stripe <124314569+jojo-stripe@users.noreply.github.com>
2023-05-23 13:50:52 -0700

    add transationcs (#24)

--- types/shared.d.ts
+++ types/shared.d.ts
@@ -41,7 +41,8 @@ export type ConnectElementTagName =
   | "stripe-connect-account-management"
   | "stripe-connect-instant-payouts"
   | "stripe-connect-payment-details"
-  | "stripe-connect-notification-banner";
+  | "stripe-connect-notification-banner"
+  | "stripe-connect-transactions";
 
 export declare const findScript: () => HTMLScriptElement | null;
 
