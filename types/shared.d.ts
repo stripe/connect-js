8a48e5789b89f1a5a05ac092deb35a2ab35739fe
Jorge Aguirre Gonzalez <95381655+jorgea-stripe@users.noreply.github.com>
2023-06-30 01:22:49 -0700

    Add new appearance options to our beta SDK (#28)

--- types/shared.d.ts
+++ types/shared.d.ts
@@ -9,7 +9,97 @@ export declare type UIConfigOptions = {
 
 export declare type AppearanceOptions = {
   colorPrimary?: string;
-  fontFamily?: string;
+  // Primary Button
+  colorPrimaryButtonBorder?: string;
+  colorPrimaryButtonBackground?: string;
+  colorPrimaryButtonText?: string;
+  // Secondary Button
+  colorSecondaryButtonBorder?: string;
+  colorSecondaryButtonBackground?: string;
+  colorSecondaryButtonText?: string;
+  // Link
+  colorPrimaryLinkText?: string;
+  colorSecondaryLinkText?: string;
+  // Border
+  colorBorder?: string;
+  // Form
+  colorFormHighlightBorder?: string;
+  colorFormAccent?: string;
+  // Text
+  colorText?: string;
+  colorSecondaryText?: string;
+  // Background
+  colorBackground?: string;
+  colorOffsetBackground?: string;
+  colorFormBackground?: string;
+
+  // Feedback Colors
+  colorDanger?: string;
+
+  // Neutral Badge Colors
+  badgeNeutralBorderColor?: string;
+  badgeNeutralBackgroundColor?: string;
+  badgeNeutralTextColor?: string;
+
+  // Success Badge Colors
+  badgeSuccessBorderColor?: string;
+  badgeSuccessBackgroundColor?: string;
+  badgeSuccessTextColor?: string;
+
+  // Warning Badge Colors
+  badgeWarningBorderColor?: string;
+  badgeWarningBackgroundColor?: string;
+  badgeWarningTextColor?: string;
+
+  // Danger Badge Colors
+  badgeDangerBorderColor?: string;
+  badgeDangerBackgroundColor?: string;
+  badgeDangerTextColor?: string;
+
+  // Border Sizing
+  borderRadius?: string;
+  borderRadiusButton?: string;
+  borderRadiusForm?: string;
+  borderRadiusBadge?: string;
+
+  // Body Typography
+  bodyLargeFontSize?: string;
+  bodyLargeFontWeight?: string;
+  bodyMediumFontSize?: string;
+  bodyMediumFontWeight?: string;
+  bodySmallFontSize?: string;
+  bodySmallFontWeight?: string;
+
+  // Label Typography
+  labelLargeFontSize?: string;
+  labelLargeFontWeight?: string;
+  labelLargeTextTransform?: string;
+  labelMediumFontSize?: string;
+  labelMediumFontWeight?: string;
+  labelMediumTextTransform?: string;
+  labelSmallFontSize?: string;
+  labelSmallFontWeight?: string;
+  labelSmallTextTransform?: string;
+
+  // Heading Typography
+  headingXLargeFontSize?: string;
+  headingXLargeFontWeight?: string;
+  headingXLargeTextTransform?: string;
+  headingLargeFontSize?: string;
+  headingLargeFontWeight?: string;
+  headingLargeTextTransform?: string;
+  headingMediumFontSize?: string;
+  headingMediumFontWeight?: string;
+  headingMediumTextTransform?: string;
+  headingSmallFontSize?: string;
+  headingSmallFontWeight?: string;
+  headingSmallTextTransform?: string;
+  headingXSmallFontSize?: string;
+  headingXSmallFontWeight?: string;
+  headingXSmallTextTransform?: string;
+
+  // Spacing
+  spacingScale?: string;
 };
 
 export type IStripeConnectUpdateParams = {
