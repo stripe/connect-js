96da2df289987a32ccfd0ccb0757fed241b9169b
Jorge Aguirre Gonzalez <95381655+jorgea-stripe@users.noreply.github.com>
2023-09-05 12:53:34 -0700

    Improve the error message for environments like nextjs

--- src/shared.ts
+++ src/shared.ts
@@ -59,6 +59,12 @@ export const loadScript = (): Promise<StripeConnectWrapper | null> => {
   }
 
   stripePromise = new Promise((resolve, reject) => {
+    if (typeof window === "undefined") {
+      reject(
+        "ConnectJs won't load when rendering code in the server - it can only be loaded on a browser. This error is expected when loading ConnectJS in SSR environments, like NextJS. It will have no impact in the UI, however if you wish to avoid it, you can switch to the `pure` version of the connect.js loader: https://github.com/stripe/connect-js#importing-loadconnect-without-side-effects."
+      );
+    }
+
     if ((window as any).StripeConnect) {
       console.warn(EXISTING_SCRIPT_MESSAGE);
     }
