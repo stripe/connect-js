8c9d48e93810680edc672e4188bdf380122dda50
Jorge Aguirre Gonzalez <95381655+jorgea-stripe@users.noreply.github.com>
2023-09-05 13:41:51 -0700

    Update shared.ts

--- src/shared.ts
+++ src/shared.ts
@@ -63,6 +63,7 @@ export const loadScript = (): Promise<StripeConnectWrapper | null> => {
       reject(
         "ConnectJs won't load when rendering code in the server - it can only be loaded on a browser. This error is expected when loading ConnectJS in SSR environments, like NextJS. It will have no impact in the UI, however if you wish to avoid it, you can switch to the `pure` version of the connect.js loader: https://github.com/stripe/connect-js#importing-loadconnect-without-side-effects."
       );
+      return;
     }
 
     if ((window as any).StripeConnect) {
