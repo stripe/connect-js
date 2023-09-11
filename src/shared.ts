ceb48f1b97461fe082d3f300e88637502ff6cfff
Joanna Li <jojo@stripe.com>
2023-09-11 15:08:56 -0400

    prettier

--- src/shared.ts
+++ src/shared.ts
@@ -2,7 +2,7 @@ import { version } from ".././package.json";
 import {
   StripeConnectWrapper,
   IStripeConnectInitParams,
-  ConnectElementTagName,
+  ConnectElementTagName
 } from "../types";
 
 export type LoadConnect = () => Promise<StripeConnectWrapper>;
@@ -20,7 +20,7 @@ const componentNameMapping: Record<
   payments: "stripe-connect-payments",
   payouts: "stripe-connect-payouts",
   "payment-details": "stripe-connect-payment-details",
-  "account-onboarding": "stripe-connect-account-onboarding",
+  "account-onboarding": "stripe-connect-account-onboarding"
 };
 
 const EXISTING_SCRIPT_MESSAGE =
@@ -125,9 +125,9 @@ const createWrapper = (stripeConnect: any) => {
           ...metaOptions,
           sdk: true,
           sdkOptions: {
-            sdkVersion: version,
-          },
-        },
+            sdkVersion: version
+          }
+        }
       });
 
       // We wrap create so we can map its different strings to supported components
@@ -142,7 +142,7 @@ const createWrapper = (stripeConnect: any) => {
         return oldCreate(htmlName);
       };
       return stripeConnectInstance;
-    },
+    }
   };
   return wrapper;
 };
