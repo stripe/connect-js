a4b54b887f689029a307699809aa5e5a93acc53a
Torrance Yang <116756196+torrance-stripe@users.noreply.github.com>
2023-12-06 17:21:05 -0500

    Add setters for fetch ephemeral key function (#87)
    * feat: add setter for fetch ephemeral key function
    
    * style: lint

--- types/config.ts
+++ types/config.ts
@@ -1,6 +1,15 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
+type FetchEphemeralKeyFunction = (fetchParams: {
+  issuingCard: string;
+  nonce: string;
+}) => Promise<{
+  issuingCard: string;
+  nonce: string;
+  ephemeralKeySecret: string;
+}>;
+
 export const ConnectElementCustomMethodConfig = {
   "payment-details": {
     setPayment: (_payment: string | undefined): void => {},
@@ -22,9 +31,14 @@ export const ConnectElementCustomMethodConfig = {
   "issuing-card": {
     setDefaultCard: (_defaultCard: string | undefined): void => {},
     setCardArtFileLink: (_cardArtFileLink: string | undefined): void => {},
-    setCardSwitching: (_cardSwitching: boolean | undefined): void => {}
+    setFetchEphemeralKey: (
+      _fetchEphemeralKey: FetchEphemeralKeyFunction | undefined
+    ): void => {}
   },
   "issuing-cards-list": {
-    setCardArtFileLink: (_cardArtFileLink: string | undefined): void => {}
+    setCardArtFileLink: (_cardArtFileLink: string | undefined): void => {},
+    setFetchEphemeralKey: (
+      _fetchEphemeralKey: FetchEphemeralKeyFunction | undefined
+    ): void => {}
   }
 };
