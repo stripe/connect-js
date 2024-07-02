135ecfd07d369618a4c4a46f7baa6726fb04a440
Kaiying Shan <kaiying@stripe.com>
2024-07-02 14:15:00 -0700

    add nullcheck initParams

--- src/pure.ts
+++ src/pure.ts
@@ -9,5 +9,10 @@ export const loadConnectAndInitialize: LoadConnectAndInitialize = (
   initParams: IStripeConnectInitParams
 ): StripeConnectInstance => {
   const maybeConnect = loadScript();
+  if (initParams == null) {
+    throw new Error(
+      "You must provide required parameters to initialize Connect"
+    );
+  }
   return initStripeConnect(maybeConnect, initParams);
 };
