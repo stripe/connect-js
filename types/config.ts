99af7578d39deb32094099ea6c0b5221393f42d5
ismacias-stripe <158622737+ismacias-stripe@users.noreply.github.com>
2024-09-12 14:17:54 -0700

    update config

--- types/config.ts
+++ types/config.ts
@@ -163,12 +163,12 @@ export const ConnectElementCustomMethodConfig = {
     setOnApplicationSubmitted: (_listener: (() => void) | undefined): void => {}
   },
   "capital-financing-promotion": {
-    setLayout: (_layout: string | undefined): void => {},
+    setLayout: (_layout: 'full' | 'banner' | undefined): void => {},
     setOnApplicationSubmitted: (
       _listener: (() => void) | undefined
     ): void => {},
     setOnEligibleFinancingOfferLoaded: (
-      _listener: (({ total }: { total: number }) => void) | undefined
+      _listener: (({ productType }: { productType: 'standard' | 'refill' | 'none' }) => void) | undefined
     ): void => {}
   }
 };
