ff99dca618aa66de553067acb25395278ba70c9c
kushalshah-stripe <109370037+kushalshah-stripe@users.noreply.github.com>
2024-09-18 12:45:43 -0700

    Added stepchange event to beta (#150)

--- types/config.ts
+++ types/config.ts
@@ -34,6 +34,10 @@ export type LoadError = {
   error: EmbeddedError;
 };
 
+export type StepChange = {
+  step: string;
+};
+
 export type EmbeddedError = {
   type: EmbeddedErrorType;
   message?: string;
@@ -99,7 +103,10 @@ export const ConnectElementCustomMethodConfig = {
     setCollectionOptions: (
       _collectionOptions: CollectionOptions | undefined
     ): void => {},
-    setOnExit: (_listener: (() => void) | undefined): void => {}
+    setOnExit: (_listener: (() => void) | undefined): void => {},
+    setOnStepChange: (
+      _listener: (({ step }: StepChange) => void) | undefined
+    ): void => {}
   },
   "account-management": {
     setCollectionOptions: (
