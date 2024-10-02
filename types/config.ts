d6c2eb7abea2e5bd1e39fe2b0ca40b29dc9d4a12
Cameron Mochrie <cmochrie@stripe.com>
2024-10-02 14:46:57 +0200

    prettier

--- types/config.ts
+++ types/config.ts
@@ -61,7 +61,7 @@ export const ConnectElementCommonMethodConfig = {
   ): void => {},
   setOnLoaderStart: (
     _listener: (({ elementTagName }: LoaderStart) => void) | undefined
-  ): void => {},
+  ): void => {}
 };
 
 export const ConnectElementCustomMethodConfig = {
@@ -82,12 +82,12 @@ export const ConnectElementCustomMethodConfig = {
     setOnExit: (_listener: (() => void) | undefined): void => {},
     setOnStepChange: (
       _listener: (({ step }: StepChange) => void) | undefined
-    ): void => {},
+    ): void => {}
   },
   "account-management": {
     setCollectionOptions: (
       _collectionOptions: CollectionOptions | undefined
-    ): void => {},
+    ): void => {}
   },
   "notification-banner": {
     setCollectionOptions: (
@@ -97,11 +97,11 @@ export const ConnectElementCustomMethodConfig = {
       _listener:
         | (({ total, actionRequired }: NotificationCount) => void)
         | undefined
-    ): void => {},
+    ): void => {}
   },
   "payment-details": {
     setPayment: (_payment: string | undefined): void => {},
-    setOnClose: (_listener: (() => void) | undefined): void => {},
+    setOnClose: (_listener: (() => void) | undefined): void => {}
   },
   "tax-settings": {
     setHideProductTaxCodeSelector: (_hidden: boolean | undefined): void => {},
@@ -110,12 +110,12 @@ export const ConnectElementCustomMethodConfig = {
     ): void => {},
     setOnTaxSettingsUpdated: (
       _listener: (({ id }: { id: string }) => void) | undefined
-    ): void => {},
+    ): void => {}
   },
   "tax-registrations": {
     setOnAfterTaxRegistrationAdded: (
       _listener: (({ id }: { id: string }) => void) | undefined
     ): void => {},
-    setDisplayCountries: (_countries: string[] | undefined): void => {},
-  },
+    setDisplayCountries: (_countries: string[] | undefined): void => {}
+  }
 };
